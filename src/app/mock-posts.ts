67e79dbeca190e9758a976f15f4c870e5ed2e178
Mael ORTIE <mael.ortie@iadinternational.com>
2019-10-08 15:31:43 +0200

    create post-list component

---
+++ src/app/mock-posts.ts
@@ -0,0 +1,8 @@
+import { Post } from './post';
+
+export const POSTS: Post[] = [
+  { title: 'Mon Premier Post', content: 'Lorem Ipsum', loveIts: 0, created_at: new Date() },
+  { title: 'Mon Deuxième Post', content: 'Lorem Ipsum', loveIts: 0, created_at: new Date() },
+  { title: 'Un autre Post', content: 'Lorem Ipsum', loveIts: 0, created_at: new Date() },
+  { title: 'Un dernier Post', content: 'Lorem Ipsum', loveIts: 0, created_at: new Date() },
+];
